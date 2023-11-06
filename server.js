ae61f42573e566680b2c777c4f4a6b246fed2cb9
nikhil0450 <nikhilkulkarni2208@gmail.com>
2023-11-06 14:59:04 +0530

    cors

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const express = require('express');
-const cors = require('cors');
+// const cors = require('cors');
 const dotenv = require('dotenv');
 const connectDB = require('./db'); 
 const app = express();
 dotenv.config(); // Load environment variables from .env file
 
-// Middleware
-const allowedOrigins = ['http://localhost:3000', 'https://6548ace362392e7bf49ab559--golden-madeleine-7367e5.netlify.app/home'];
-app.use(cors({ origin: allowedOrigins }));
+// // Middleware
+// const allowedOrigins = ['http://localhost:3000', 'https://6548ace362392e7bf49ab559--golden-madeleine-7367e5.netlify.app/home'];
+// app.use(cors({ origin: allowedOrigins }));
 app.use(express.json());
 
 // Connect to MongoDB
