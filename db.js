bb49d0156a6f03edbb9ee8b095483e3f3515064d
nikhil0450 <nikhilkulkarni2208@gmail.com>
2023-11-06 13:35:13 +0530

    procfile & minor changes

--- db.js
+++ db.js
@@ -3,7 +3,10 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
   console.log(process.env.NODE_ENVIRONMENT);
   try {
-    const conn = await mongoose.connect(process.env.MONGODB_URI);
+    const conn = await mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     console.log(`Connected to Database`);
   } catch (error) {
     console.error(`Error connecting to Database`);
--- server.js
+++ server.js
@@ -3,13 +3,13 @@ const cors = require('cors');
 const dotenv = require('dotenv');
 const connectDB = require('./db'); 
 const app = express();
-dotenv.config();
+dotenv.config(); // Load environment variables from .env file
 
 // Middleware
 app.use(
   cors({
-    "origin": "*",
-    "methods": "GET,PUT,POST"
+    origin: process.env.ALLOWED_ORIGIN, // Update this to use your CORS origin
+    methods: 'GET,PUT,POST',
   })
 );
 app.use(express.json());
@@ -18,7 +18,6 @@ app.use(express.json());
 connectDB();
 
 // Routes
-
 const userRoutes = require('./routes/userRoutes');
 const authRoutes = require('./routes/authRoutes');
 const bmiRoutes = require('./routes/bmiRoutes');
@@ -27,7 +26,7 @@ app.use('/api/auth', authRoutes);
 app.use('/api/bmi', bmiRoutes);
 app.use('/api/user', userRoutes);
 
-const PORT = process.env.PORT;
-app.listen(PORT, process.env.HOSTNAME , () => {
+const PORT = process.env.PORT || 5000; // Use process.env.PORT for the port
+app.listen(PORT, process.env.HOSTNAME, () => {
   console.log(`Server is running on port ${PORT}`);
 });
